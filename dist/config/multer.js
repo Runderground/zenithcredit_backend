32cb9d34679812b3e47dcc9c4fc47751d51ae5ca
FuturoDev-jpg <onkgodz@gmail.com>
2024-12-01 15:44:09 +0000

    fix: multer config

--- dist/config/multer.js
+++ dist/config/multer.js
@@ -12,8 +12,8 @@ dotenv_1.default.config();
 const s3 = new client_s3_1.S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     }
 });
 const deleteImageFromS3 = async (imageKey) => {
